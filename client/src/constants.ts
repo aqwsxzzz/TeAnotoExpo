b5a4bb3b7363ad5256d2ff2dd56db09c4de2b35f
Jorge <jbarretolarrosa@gmail.com>
2024-09-20 11:32:28 -0300

    Create constants file for development generic keys

---
+++ client/src/constants.ts
@@ -0,0 +1,3 @@
+export const baseUrl = "http://192.168.1.15:3000";
+export const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcyNjc4MjgyMSwiZXhwIjoxNzI2ODY5MjIxfQ.QVv-lmFlIZYnz729hR_66HNbUs45jfPMTwxLcTaR2sI";
